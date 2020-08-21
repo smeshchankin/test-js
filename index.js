de869ffa680ff8d5900d4e39bf312c2d837fc29c
Sergey Meshchankin <smeshchankin@gmail.com>
2020-08-21 21:12:58 +0300

    [TJS-001] Add tests for assert.equals

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+(function() {
+    function sum(x, y) {
+        return x + y;
+    }
 
+    assert.equals(5, sum(2, 3), 'Test sum of numbers 2 and 3');
+    assert.equals('5', sum(2, 3), 'Test sum of numbers 2 and 3 !== \'5\'');
+    assert.equals(6, sum(2, 3), 'Test sum of numbers 2 and 3 != 6');
+}());
